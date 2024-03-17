fb0f64c5dae22c2f63a5e5c1f538124c3c3ab7cf
Mihir Mistry <mihir.mistry308@gmail.com>
2024-03-17 16:43:01 +0530

    Init firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase : {
+    apiKey: "AIzaSyBk0IkCN1pa4Y2yER1EXnu1hrLtbhu41T4",
+    authDomain: "test-2839e.firebaseapp.com",
+    projectId: "test-2839e",
+    storageBucket: "test-2839e.appspot.com",
+    messagingSenderId: "780334271089",
+    appId: "1:780334271089:web:ab384c1ab1ddb0615429d0",
+    measurementId: "G-DCSVKE63GL"
+  }
+};
+
